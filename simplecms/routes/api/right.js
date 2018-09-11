03f457f397c9f59810abd13951a750d9c20b19aa
yongjia_c <yjtoro@hotmail.com>
2018-09-11 23:01:49 +0800

    completed right crud

--- simplecms/routes/api/right.js
+++ simplecms/routes/api/right.js
@@ -48,31 +48,66 @@ router.get('/:roleid', isAuthenticated, (req, res, next) => {
 
 //POST create new rights, 
 //params @JSON.stringify array of objects
-router.post('/', isAuthenticated, (req, res, next) => {
+router.patch('/:rightid', isAuthenticated, (req, res, next) => {
   var action = "write";
-  
+  if(! security.isUUID(req.params.rightid)){
+    var response = new resp();
+    res.status(400).send(response.initResp(null, {
+      msg: 'Invalid ID pass in!',
+      code: 400,
+      status: true
+    }));
+  }
+
   if(res.auth_token !== null){
-    var role = res.auth_token.right;
+    var role = res.auth_token.role;
     var validPermit = security.permit(action, role.right.acl);
     if (validPermit) {
-      var mRight = JSON.parse(req.body.rights);
+      //var mRight = JSON.parse(req.body.rights);
+      var mRight = {
+        RightID: req.params.rightid
+      };
 
-      security.validateMore(mRight, (err, mRightValidated) => {
+      var mRightUpdate = {};
+      for(var key in req.body){
+        if(req.body.hasOwnProperty(key)){
+          if(key.toUpperCase() === "MODULE"){
+            mRightUpdate['module'] = {
+              val: req.body.module,
+                type: 'string',
+                check: true
+            }
+          }else if(key.toUpperCase() === "SECTION"){
+            mRightUpdate['section'] = {
+              val: req.body.section,
+              type: 'string',
+              check: true 
+            }
+          }else if(key.toUpperCase() === "ACL"){
+            mRightUpdate['acl'] = {
+              val: req.body.acl,
+              type: 'integer',
+              check: true
+            }
+          }
+        }
+      }
+
+      security.validate(mRightUpdate, (err, mRightValidated) => {
         if(err) next(err);
         var response = new resp();
 
         if(mRightValidated !== null){
-          Right.bulkCreate(mRightValidated)
-            .then(affectedRows => {
-              
-              if(affectedRows.length > 0){                              
-                response.initResp(affectedRows);
-              }else{
-                response.initResp(null, {
-                  msg: 'No rights is created'
-                });
-              }
-              res.status(200).send(response);
+          Right.findOne({
+            where: mRight
+          }).then(right => {
+              right.update(mRightValidated).then(mRightUpdated =>{
+                response.initResp(mRightUpdated);
+                res.status(200).send(response);
+              }).catch((e) => {
+                response.initResp(null, e);
+                res.status(400).send(response);
+              });                                          
           });
         }else{
           res.status(400).send(null, {
@@ -89,12 +124,80 @@ router.post('/', isAuthenticated, (req, res, next) => {
   }
 });
 
-router.patch('/:rightid', (req, res, next) => {
+router.post('/', isAuthenticated, (req, res, next) => {
+  var action = "write";
 
+  if (res.auth_token !== null) {
+    var role = res.auth_token.role;
+    var validPermit = security.permit(action, role.right.acl);
+    if (validPermit) {
+      var mRight = JSON.parse(req.body.rights);
+
+      security.validateMore(mRight, (err, mRightValidated) => {
+        if (err) next(err);
+        var response = new resp();
+
+        if (mRightValidated !== null) {
+          Right.bulkCreate(mRightValidated)
+            .then(affectedRows => {
+
+              if (affectedRows.length > 0) {
+                response.initResp(affectedRows);
+              } else {
+                response.initResp(null, {
+                  msg: 'No rights is created'
+                });
+              }
+              res.status(200).send(response);
+            });
+        } else {
+          res.status(400).send(null, {
+            msg: 'Sequelize cannot create right object!',
+            code: 400,
+            status: true
+          });
+        }
+      });
+    }
+  } else {
+    var response = new resp();
+    res.status(400).send(response.unAuthResp());
+  }
 });
 
-router.delete('/', (req, res, next) => {
+router.delete('/', isAuthenticated, (req, res, next) => {
+  var action = "delete";
+  var response = new resp();
 
+  if(res.auth_token !== null){
+    var role = res.auth_token.role;
+    var validPermit = security.permit(action, role.right.acl);
+    if(validPermit){
+      if(security.isUUID(req.body.right_id)){
+        var mRight = {
+          RightID: req.body.right_id
+        };
+
+        Right.findOne({
+          where: mRight
+        }).then(right => {
+          if(right !== null){
+            right.destroy();
+          }
+          res.status(200).send(response.initResp("Successfully deleted this right!",
+            null, {
+              msg: 'successfully deleted admin',
+              code: 200,
+              status: true
+            }));
+        });
+      }
+    }else{
+      res.status(401).send(response.unAuthResp());
+    }
+  }else{
+    res.status(401).send(response.unAuthResp());
+  }
 });
 
 function isAuthenticated(req, res, next) {
